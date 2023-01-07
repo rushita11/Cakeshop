95dd84ce0ec8362c89ea6336b43cf2e3830ae1fc
rushita11 <rushitathummar123@gmail.com>
2023-01-07 08:47:57 +0530

    cake

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header';
+import Footer from './Component/Footer';
+import Home from './Container/Home';
+import About from './Container/About';
+import { Route } from 'react-router-dom';
+import Contact from './Container/Contact';
+import Service from './Container/Service';
+import Testmonial from './Container/Testmonial';
+import { Switch } from 'react-router-dom';
+import Team from './Container/Team';
+import Menu from './Container/Menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Switch>
+    {/* <Home /> */}
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/contact" component={Contact} />
+    <Route exact path="/service" component={Service} />
+    <Route exact path="/testmonial" component={Testmonial} />
+    <Route exact path="/team" component={Team} />
+    <Route exact path="/menu" component={Menu} />
+    </Switch>
+    <Footer />
+    </>
   );
 }
 
